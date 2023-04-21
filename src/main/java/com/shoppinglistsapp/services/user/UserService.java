c163e18a557c80cecd987ff30a129330485aca9f
Justyna <justyna.slubowska1@gmail.com>
2023-04-21 14:24:57 +0200

    ok

---
+++ src/main/java/com/shoppinglistsapp/services/user/UserService.java
@@ -0,0 +1,9 @@
+package com.shoppinglistsapp.services.user;
+
+import com.shoppinglistsapp.model.entity.User;
+
+public interface UserService {
+
+    User saveUser(User user);
+
+}
---
+++ src/main/java/com/shoppinglistsapp/services/user/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.shoppinglistsapp.services.user;
+
+import com.shoppinglistsapp.model.entity.User;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+}
